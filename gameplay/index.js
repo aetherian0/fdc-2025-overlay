88bf3b35ebff8019125c7f77def3504b1b2aeaa2
Aetherian <saarinenotso@gmail.com>
2024-12-18 18:40:16 +0200

    Update index.js

--- gameplay/index.js
+++ gameplay/index.js
@@ -1,16 +1,18 @@
-const socket = new ReconnectingWebSocket('ws://' + location.host + '/websocket/v2');
+const socket = new ReconnectingWebSocket(
+    "ws://" + location.host + "/websocket/v2"
+);
 const cache = {};
 
-socket.onmessage = async event => {
+socket.onmessage = async (event) => {
     const data = JSON.parse(event.data);
 
     if (cache.scoreVisible !== data.tourney.scoreVisible) {
         cache.scoreVisible = data.tourney.scoreVisible;
 
         if (cache.scoreVisible) {
-            $('#bottomright').addClass('blue').removeClass('red');
+            $("#bottomright").addClass("blue").removeClass("red");
         } else {
-            $('#bottomright').addClass('red').removeClass('blue')
+            $("#bottomright").addClass("red").removeClass("blue");
         }
     }
-}
+};
